5c4bf1bd04cfe37515de270470b02f110db11eb4
vigta <vigta257@gmail.com>
2025-01-03 17:17:56 +0100

    First project files

---
+++ public/script.js
@@ -0,0 +1,16 @@
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function appendValue(value) {
+    document.getElementById('result').value += value;
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
